7a81138941272c3237e201ff14b70134bac65970
Tom Hill <thill@cpmagic.com>
2018-09-29 16:54:36 -0400

    FirstCommit

---
+++ Concentration/Concentration.swift
@@ -0,0 +1,27 @@
+//
+//  Concentration.swift
+//  Concentration
+//
+//  Created by Thomas Hill on 9/29/18.
+//  Copyright © 2018 Computer Magic. All rights reserved.
+//
+
+import Foundation
+
+class Concentration {
+    
+    var cards = [Card]()
+    
+    func chooseCard(at index: Int) {
+        
+    }
+    
+    init(numberOfPairsOfCards: Int) {
+        for _ in 1...numberOfPairsOfCards {
+            let card = Card()
+            cards += [card, card]
+        }
+        // TODO: Shuffle the cards
+    }
+    
+}
