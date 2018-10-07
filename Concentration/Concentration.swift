791552b09646a904ed6e150d4c1f9fb307a8aa35
Tom Hill <thill@cpmagic.com>
2018-10-07 13:38:13 -0400

    Up to Shuffle the cards

--- Concentration/Concentration.swift
+++ Concentration/Concentration.swift
@@ -9,5 +9,17 @@
 import Foundation
 
 class Concentration {
+    var cards = [Card]()
     
+    func chooseCard(at index: Int) {
+        
+    }
+    
+    init(numberOfPairsOfCards: Int) {
+        for _ in 1...numberOfPairsOfCards {
+            let card = Card()
+            cards += [card, card]
+        }
+        // TODO: Shuffle the cards
+    }
 }
